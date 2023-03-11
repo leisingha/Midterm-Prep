9d33501b23d9158a3f7452c50e552ab844547bb8
lei <120764124+leisingha@users.noreply.github.com>
2023-03-10 23:59:18 -0500

    Fixed linter's complaints

--- sample.py
+++ sample.py
@@ -1,2 +1,11 @@
-def foo(a):
-  return a + 2
+""" 
+Helper function live here
+"""
+
+def increment_by_two(var_to_increment):
+    """
+    This function increments input by two
+    :param var_to_increment: param to increment
+    :return incremented variable
+    """
+    return var_to_increment + 2
